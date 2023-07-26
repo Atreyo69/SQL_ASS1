b44a905803540fcf80d8fedeca7047e810f26277
Atreyo Mandal <50211770+Atreyo69@users.noreply.github.com>
2023-07-26 20:03:51 +0530

    Add files via upload

---
+++ Ass1.sql
@@ -0,0 +1,25 @@
+use todolist;
+
+create table todol
+(
+task_id int primary key, 
+task_name varchar(255) not null,
+ description text,
+ is_completed boolean not null default false
+ );
+
+insert into todol (task_id,task_name, description, is_completed) VALUES
+    (101,'Task 1', 'Wake up at 9am.', true),
+    (102,'Task 2', 'Having coffee.', false),
+    (103,'Task 3', 'Having a shower.', false),
+    (104,'Task 4', 'Brushing teeths.', true),
+    (105,'Task 5', 'Playing games.', false);
+    
+ 
+SELECT * FROM todol;
+
+
+SELECT * FROM todol WHERE is_completed = true;
+
+
+SELECT * FROM todol WHERE is_completed = false;
